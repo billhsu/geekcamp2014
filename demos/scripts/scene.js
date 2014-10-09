c0ae929f825a4a3dc32311f266aff6d2a515f28c
Shipeng Xu <billhsu.x@gmail.com>
2014-10-09 23:15:56 +0800

    working on scene

--- demos/scripts/scene.js
+++ demos/scripts/scene.js
@@ -1,26 +1,7 @@
+// Shipeng Xu 2014
 var demo = new CANNON.Demo();
 
 demo.addScene("quadcopter", function() {
-    var world = setupWorld(demo);
-    var shape1 = new CANNON.Box(new CANNON.Vec3(10, 1, 0.5));
-    var shape2 = new CANNON.Box(new CANNON.Vec3(1, 10, 0.5));
-    var shape3 = new CANNON.Box(new CANNON.Vec3(5, 5, 0.5));
-    var mass = 10;
-    var body = new CANNON.Body({
-        mass: mass
-    });
-    body.position.set(0, 0, 6);
-    body.quaternion.setFromAxisAngle(new CANNON.Vec3(0, 0, 0), Math.PI / 32);
-
-    body.addShape(shape1, new CANNON.Vec3(-15, 0, 0));
-    body.addShape(shape1, new CANNON.Vec3(15, 0, 0));
-    body.addShape(shape2, new CANNON.Vec3(0, -15, 0));
-    body.addShape(shape2, new CANNON.Vec3(0, 15, 0));
-    body.addShape(shape3, new CANNON.Vec3(0, 0, 0));
-    demo.addVisual(body);
-});
-
-function setupWorld(demo) {
     var world = demo.getWorld();
     world.gravity.set(0, 0, -9.8);
     world.broadphase = new CANNON.NaiveBroadphase();
@@ -34,8 +15,48 @@ function setupWorld(demo) {
     groundBody.addShape(groundShape);
     world.add(groundBody);
     demo.addVisual(groundBody);
+    var length_wing = 5;
+    var size_board = 2.5;
+    var shape1 = new CANNON.Box(new CANNON.Vec3(length_wing, 0.5, 0.25));
+    var shape2 = new CANNON.Box(new CANNON.Vec3(0.5, length_wing, 0.25));
+    var shape3 = new CANNON.Box(new CANNON.Vec3(size_board, size_board, 0.25));
+    var mass = 10;
+    var body = new CANNON.Body({
+        mass: mass
+    });
+    body.position.set(0, 0, 7);
+    body.quaternion.setFromAxisAngle(new CANNON.Vec3(0, 0, 0), Math.PI / 32);
 
-    return world;
-};
+    body.addShape(shape1, new CANNON.Vec3(-(length_wing + size_board), 0, 0));
+    body.addShape(shape1, new CANNON.Vec3((length_wing + size_board), 0, 0));
+    body.addShape(shape2, new CANNON.Vec3(0, -(length_wing + size_board), 0));
+    body.addShape(shape2, new CANNON.Vec3(0, (length_wing + size_board), 0));
+    body.addShape(shape3, new CANNON.Vec3(0, 0, 0));
+    world.add(body);
+    demo.addVisual(body);
+
+    var bodyRef = new CANNON.Body();
+    bodyRef.position.set(0, 0, 7);
+    var constraints = [];
+    bodyRef.quaternion.setFromAxisAngle(new CANNON.Vec3(0, 0, 0), Math.PI / 32);
+    world.add(bodyRef);
+
+    // constraints.push(new CANNON.HingeConstraint(body, bodyRef, {
+    //     pivotA: new CANNON.Vec3(),
+    //     axisA: new CANNON.Vec3(0, 1, 0),
+    //     pivotB: new CANNON.Vec3(),
+    //     axisB: new CANNON.Vec3(0, 0, 1)
+    // }));
+
+    constraints.push(new CANNON.PointToPointConstraint(body, new CANNON.Vec3(0, 5, 7), groundBody, new CANNON.Vec3(0, 5, 0)));
+    constraints.push(new CANNON.PointToPointConstraint(body, new CANNON.Vec3(0, -5, 7), groundBody, new CANNON.Vec3(0, -5, 0)));
+    for (var i = 0; i < constraints.length; i++)
+        world.addConstraint(constraints[i]);
+
+    body.applyImpulse(new CANNON.Vec3(0, 0, 10), new CANNON.Vec3(4, 0, 0));
+    //demo.bodies[1].applyForce(new CANNON.Vec3(0, 10, 19000), new CANNON.Vec3(-4, 0, 0));
+
+});
+
 
 demo.start();
